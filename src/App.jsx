d2c8af81e653b1c1588f9a79d0f6394d2df0a8f1
lakshman5615 <lakshmanjoshiji@gmail.com>
2026-02-21 15:08:16 +0530

    Frontend folder strucuture setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white p-8 rounded-2xl shadow-lg text-center">
+        <h1 className="text-3xl font-bold text-blue-600 mb-4">
+          Tailwind v4 Working 🚀
+        </h1>
+        <button className="px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600 transition">
+          Click Me
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
