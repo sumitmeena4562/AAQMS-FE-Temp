1bf87cd28bf50d0dd115b961512653669d4f243a
lakshman5615 <lakshmanjoshiji@gmail.com>
2026-03-18 11:26:15 +0530

    : add stat-card box shadow utility.

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ export default {
             boxShadow: {
                 'figma': '0px 1px 2px 0px rgba(0, 0, 0, 0.05)',
                 'card': '0px 2px 18px -2px #10224F60',
+                'stat-card': '0px 0px 16.6px -4px rgba(16, 34, 79, 0.25)',
             }
         },
     },
